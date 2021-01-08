0e52de93617875214f20690e76e935be009543c5
Nadine Delaire <ndelaire16@gmail.com>
2021-01-07 19:50:41 -0800

    added to api routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -9,9 +9,15 @@ module.exports = function(app) {
   app.post("/api/notes", function (req, res) {
     const newNote = req.body;
 
-    console.log(newNote);
+// Missing something -- does this need a push? An if statement? I think something else is still missing. And does the below fs work? 
 
-  })
+fs.writeFile(__dirname + "./db/db.json", function(err, data) {
+  if (err) throw err;
+
+ console.log("Note added")
+ res.send();
+});
+})
 
   app.delete("api/notes/:id", function (req, res) {
 
